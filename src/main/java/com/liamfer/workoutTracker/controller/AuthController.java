f53d13a5642d92b060f9194b2a6854570e5cdee9
William Fernandes <wiilfern1910@outlook.com>
2025-06-25 12:46:07 -0300

    feat: Configurações Spring Security

---
+++ src/main/java/com/liamfer/workoutTracker/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.liamfer.workoutTracker.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @PostMapping("/register")
+    public ResponseEntity<String> authRegister(){
+        return ResponseEntity.status(HttpStatus.CREATED).body("hoila");
+    }
+}
